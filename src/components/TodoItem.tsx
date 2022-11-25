80a1ede02e934e22ec7df07ffa4e30acb6856e58
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-11-26 08:16:28 +0900

    大まかなpropsを定義

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,12 +1,30 @@
 import { BsTrash } from "react-icons/bs";
 import { FiEdit } from "react-icons/fi";
-export const TodoItem = () => {
+
+interface Props {
+  label: string;
+  onClickEditIcon: () => void;
+  onClickTrashIcon: () => void;
+}
+
+/**
+ * Todo表示
+ * @param {string} label Todoの内容
+ * @param onClickEditIcon 編集ボタンを押したときの処理
+ * @param onClickTrashIcon ゴミ箱ボタンを押したときの処理
+ */
+/** */
+export const TodoItem = ({
+  label,
+  onClickEditIcon,
+  onClickTrashIcon,
+}: Props) => {
   return (
     <li className="flex items-center p-8 text-lg font-bold bg-white justify-between">
-      <span>todo1</span>
+      <span>{label}</span>
       <div className="flex items-center space-x-2">
-        <FiEdit className="cursor-pointer" />
-        <BsTrash className="cursor-pointer" />
+        <FiEdit onClick={onClickEditIcon} className="cursor-pointer" />
+        <BsTrash onClick={onClickTrashIcon} className="cursor-pointer" />
       </div>
     </li>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import { InputForm } from "../components/InputForm";
 import { TodoItem } from "../components/TodoItem";
 
 //トップページ
 export const Index = () => {
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const handleEditTodo = () => {
+    console.log("edit");
+  };
+  const handleDeleteTodo = () => {
+    console.log("delete");
+  };
+
   return (
     <div>
       <h1 className="text-center text-2xl font-bold">TodoList</h1>
       <div className="pt-10">
-        <InputForm placeholder="search todo" />
+        <InputForm
+          placeholder="search todo"
+          onClick={() => console.log("aaa")}
+        />
       </div>
       <ul className="space-y-4 pt-10">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo}
+            label={""}
+            onClickEditIcon={handleEditTodo}
+            onClickTrashIcon={handleDeleteTodo}
+          />
+        ))}
       </ul>
     </div>
   );

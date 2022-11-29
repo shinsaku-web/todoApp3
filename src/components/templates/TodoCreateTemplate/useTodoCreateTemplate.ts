8cdf05528be2c884fb24fafb23cfc8bedb24f6be
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-11-29 09:34:09 +0900

    createの作成

--- src/components/templates/TodoCreateTemplate/useTodoCreateTemplate.ts
+++ src/components/templates/TodoCreateTemplate/useTodoCreateTemplate.ts
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { SERVER_BASE_PATH } from "../../../constants/constants";
 import { TodoType } from "../../../types/TodoType";
 
 export const useTodoCreateTemplate = () => {
@@ -27,28 +26,11 @@ export const useTodoCreateTemplate = () => {
       contents: newContents,
     }));
   };
-  const handleSubmit = async () => {
-    try {
-      const response = await fetch(SERVER_BASE_PATH + "/todos", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(todo),
-      });
-      if (!response.ok) {
-        throw new Error("エラー発生");
-      }
-      navigate("/");
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   return {
+    todo,
     navigateToTop,
     handleChangeTitle,
     handleChangeContents,
-    handleSubmit,
   };
 };
